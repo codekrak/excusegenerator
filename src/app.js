52fbe3ad7b38ccb9f6fedb12a0f8ce56765ebb23
codekrak <codekrak68@gmail.com>
2021-05-26 22:16:59 +0000

    initial push

--- src/app.js
+++ src/app.js
@@ -7,5 +7,27 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let indexWho = Math.floor(Math.random() * who.length);
+  let indexAction = Math.floor(Math.random() * action.length);
+  let indexWhat = Math.floor(Math.random() * what.length);
+  let indexWhen = Math.floor(Math.random() * when.length);
+  let finalString =
+    who[indexWho] +
+    " " +
+    action[indexAction] +
+    " " +
+    what[indexWhat] +
+    " " +
+    when[indexWhen];
+  document.querySelector("h2").innerHTML = finalString;
 };
